9c19874e512df25f9b352c23a5f381f148603d5b
Henrik Hoangkhanh Huynh <202307381@post.au.dk>
2025-05-26 18:43:56 +0200

    Done with about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactLogo from '../assets/react.svg';
+import head from '../assets/CV_pic.png';
 import { RevealOnScroll } from './RevealOnScroll';
 import '../index.css';
 
@@ -12,15 +12,18 @@ export const About = () => {
                         <div className="max-w-xl">
                             <h1 className="text-4xl font-bold text-[var(--headerText)] mb-4">About</h1>
                             <p className="text-xl text-bodyText">
-                                I'm a computer engineering student doing something with my life ig
+                                I'm a computer engineering student from Denmark with a passion for technology and problem-solving. 
+                                I love learning new skills both for software development and leisure. 
+                                When I'm not working on any projects, I like playing piano and guitar, gaming, or climbing some problems in a bouldering gym. 
+                                This is my try at making a professional looking website so I you enjoy reading or just looking through
                             </p>
                         </div>
                         <div className="mt-8 md:mb-0 md:ml-8">
-                            <div className="w-64 h-64 bg-[var(--cardBackground)] rounded-full flex items-center justify-center">
+                            <div className="w-128 h-128 bg-[#b4a399] rounded-full flex items-center justify-center overflow-hidden">
                                 <img
-                                    src={ReactLogo}
+                                    src={head}
                                     alt="Profile"
-                                    className="w-48 h-48 object-contain"
+                                    className="w-128 h-128 object-contain"
                                 />
                             </div>
                         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,8 @@ export const Home = () => {
                 <div className="w-screen h-screen bg-radial-ellipse flex flex-col items-center justify-center">
                     {/* Main Content */}
                     <div className="flex flex-col items-center">
-                        <h1 className="text-5xl md:text-6xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[var(--mainTitleSecond)] to-[var(--headerText)]">
-                            Hello, I'm Henrik
+                        <h1 className="text-5xl md:text-6xl font-semibold leading-tight bg-clip-text text-transparent bg-gradient-to-r from-[var(--mainTitleSecond)] to-[var(--headerText)]">
+                            Hey, I'm Henrik
                         </h1>
                         <a href={CV} download="CV_Henrik_Huynh" className="mt-8">
                             <button className="px-6 py-3 bg-[var(--headerText)]/50 rounded-xl text-[var(--bodyText)] text-lg shadow-md hover:bg-[var(--headerText)] transition cursor-pointer">
